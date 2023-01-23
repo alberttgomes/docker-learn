657007c97913414b6a672fe0e565be1445ed1fe3
Albert Gomes <albert.gomes@liferay.com>
2023-01-23 17:40:22 -0300

    Last changes in docker files

--- database_dump.sql
+++ database_dump.sql
@@ -1,12 +1,12 @@
 CREATE DATABASE database_dump;
 USE database_dump;
 
-CREATE TABLE students(
-    StudentID int not null AUTO_INCREMENT,
+CREATE TABLE students (
+    StudentID INT NOT NULL AUTO_INCREMENT,
     FirstName VARCHAR(100) NOT NULL,
     LastName VARCHAR(100) NOT NULL,
     PRIMARY KEY (StudentID)
 )
 
-INSERT INTO students (StudentID, FirstName, LastName) 
-VALUES("Albert","Gomes"), ("Luna", "Smith");
+-- INSERT INTO students (FirstName, LastName) 
+-- VALUES ('Albert','Gomes'), ('Luna', 'Smith');
