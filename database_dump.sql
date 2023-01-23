23341a44c7e9fff1304fef84a9c4c9105603b918
Albert Gomes <albert.gomes@liferay.com>
2023-01-23 18:25:33 -0300

    Fix database query

--- database_dump.sql
+++ database_dump.sql
@@ -2,11 +2,11 @@ CREATE DATABASE database_dump;
 USE database_dump;
 
 CREATE TABLE students (
-    StudentID INT NOT NULL AUTO_INCREMENT,
+    StudentID INTEGER NOT NULL AUTO_INCREMENT,
     FirstName VARCHAR(100) NOT NULL,
     LastName VARCHAR(100) NOT NULL,
     PRIMARY KEY (StudentID)
-)
+);
 
--- INSERT INTO students (FirstName, LastName) 
--- VALUES ('Albert','Gomes'), ('Luna', 'Smith');
+INSERT INTO students (FirstName, LastName) 
+VALUES ('Albert','Gomes'), ('Luna', 'Smith');
