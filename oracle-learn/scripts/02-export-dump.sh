50c42d62c30c8bb9e9dc319230ec212692c742a0
Albert Gomes <albert.gomes@liferay.com>
2023-05-08 17:19:29 -0300

    Refactory export in dockerfile and script

--- oracle-learn/scripts/02-export-dump.sh
+++ oracle-learn/scripts/02-export-dump.sh
@@ -4,8 +4,9 @@ echo "RUNNING..."
 
 export ORACLE_SID=ORCLCDB
 
-echo "Init Export..."
-expd User_/Root1234@ORCLCDB DIRECTORY=IMP_DIR DUMPFILE=DumpFile.dmp LOGFILE=DumpLogFile.log EXCLUDE=STATISTICS
+echo "INIT EXPORT DUMP..."
+
+expdp User_/Root1234@ORCLCDB DIRECTORY=IMP_DIR DUMPFILE=dumpmysql.sql LOGFILE=DumpLogFile.log EXCLUDE=STATISTICS
 
 echo "Disabling OOB..."
 echo "DISABLE_OOB=ON" >> /opt/oracle/product/19c/dbhome_1/network/admin/sqlnet.ora
