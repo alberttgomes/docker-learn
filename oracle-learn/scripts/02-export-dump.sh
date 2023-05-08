bc38809557462e9576ebcb6aaf70e3ba0899a9f3
Albert Gomes <albert.gomes@liferay.com>
2023-05-08 19:32:12 -0300

    SF

--- oracle-learn/scripts/02-export-dump.sh
+++ oracle-learn/scripts/02-export-dump.sh
@@ -5,7 +5,6 @@ echo "RUNNING..."
 export ORACLE_SID=ORCLCDB
 
 echo "INIT EXPORT DUMP..."
-
 expdp User_/Root1234@ORCLCDB DIRECTORY=IMP_DIR DUMPFILE=dumpmysql.sql LOGFILE=DumpLogFile.log EXCLUDE=STATISTICS
 
 echo "Disabling OOB..."
